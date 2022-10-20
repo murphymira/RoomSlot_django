30c09780ac346f44bbae0d0410bc29bcdf8cb66b
Mustapha Moruf <mumurphymira20@gmail.com>
2022-10-20 16:14:59 +0100

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets
+from login.models import Customer
+from booking.models import Booking, Rooms
+from . import serializers
+
+
+class CustomerViewset(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class RoomsViewset(viewsets.ModelViewSet):
+    queryset = Rooms.objects.all()
+    serializer_class = serializers.RoomsSerializer
+
+
+class BookingViewset(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = serializers.BookingSerializer
